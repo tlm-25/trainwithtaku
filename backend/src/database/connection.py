77cb47c070533a44d2d9d41c65344156ef0d60fd
Taku <tlm2g18@soton.ac.uk>
2025-12-24 15:09:52 +0000

    successfull connection test

--- backend/src/database/connection.py
+++ backend/src/database/connection.py
@@ -1,7 +1,28 @@
-from pymongo import AsyncMongoClient
+from pymongo import AsyncMongoClient, MongoClient
 import os 
 from src.config import MONGO_DB_CONNECTION_STRING
+import pytest_asyncio
+import logging
+#mark with pytest_asyncio so that the result can be passed into pytest function for testing - dependency injection - makes the client accessible in async tests
+@pytest_asyncio.fixture
+async def get_mongo_client():
+    '''
+        Open connection to mongoDB database
+    '''
+    try:
+        # using 'async with' and 'yield' so that the client is available to use in a another function, and depedency injection used to access the connection while it's open:  
+        # Makes connection useable in fastAPI endpoints 
+        logging.info("connecting to databse")
+        async with AsyncMongoClient(host=MONGO_DB_CONNECTION_STRING,serverSelectionTimeoutMS=10000) as mongo_client:
+            
+            result = await mongo_client.admin.command('ping')
+            print(result)
+            print(type(mongo_client))
+            logging.info("succesfully connected to database")
+            yield mongo_client,result
+            
+        
+    except Exception as e:
+        yield f"failed to connect {e}"
 
-
-MONGO_CLIENT = AsyncMongoClient(host=MONGO_DB_CONNECTION_STRING)
 
--- backend/tests/integration/test_connection.py
+++ backend/tests/integration/test_connection.py
@@ -1,2 +1,21 @@
 #test the database connection
 
+from src.database.connection import get_mongo_client
+
+import pytest_asyncio
+import pytest
+from pymongo import AsyncMongoClient
+from pymongo.errors import ConnectionFailure
+import logging
+
+
+#testing the connection has been successful - signal to pytest that it's an async function 
+@pytest.mark.asyncio
+async def test_successful_database_connetion(get_mongo_client):
+    #attempt to connect to database
+    # mongo_client =  connect_to_database()
+    assert isinstance(get_mongo_client[0],AsyncMongoClient)
+    assert get_mongo_client[1]["ok"] == 1.0
+    
+    
+
