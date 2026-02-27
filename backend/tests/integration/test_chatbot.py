c03b33183cd9e465bcb3221bb9eda6eb66c7dcff
Taku <tlm2g18@soton.ac.uk>
2026-02-27 11:20:24 +0000

    tidy up

--- backend/tests/integration/test_chatbot.py
+++ backend/tests/integration/test_chatbot.py
@@ -79,7 +79,7 @@ async def test_create_new_chat():
 
         create_new_chat_response = client.post(url=f"/create_new_chat",headers=headers)
         create_new_chat_response_json = create_new_chat_response.json()
-        print(create_new_chat_response_json)
+
         assert create_new_chat_response.status_code == 201
         assert "conversation_id" in create_new_chat_response_json
 
