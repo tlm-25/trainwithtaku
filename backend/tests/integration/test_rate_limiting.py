6b57244e2567445fecc8de5236caa9c8b9596d63
Taku <tlm2g18@soton.ac.uk>
2026-05-24 17:08:58 +0100

    run rate_limit tests in isolation

--- backend/tests/integration/test_rate_limiting.py
+++ backend/tests/integration/test_rate_limiting.py
@@ -56,8 +56,11 @@ def login_test_user(test_client):
     return {"Authorization": f"Bearer {login_response.json()['access_token']}"}
 
 
+
+
 @pytest.mark.asyncio
 @pytest.mark.slow
+@pytest.mark.rate_limit
 async def test_simulate_brute_force_attack(test_client):
     '''
         Simulate brute force attack where an attacker is trying to guess a password
@@ -96,9 +99,10 @@ async def test_simulate_brute_force_attack(test_client):
 @pytest.mark.asyncio    
 @pytest.mark.llm_call
 @pytest.mark.slow
+@pytest.mark.rate_limit
 async def test_user_chatbot_rate_limit(test_client,login_test_user):
     '''
-    Confirm that user based chat limiting enforced 
+    Confirm that user based chat limiting enforced - RUN IN ISOLATION TO AVOID STATE CORRUPTION
     
     '''
     n_iter = _get_n_iterations_from_rate_limit(rate_limit_string=RATE_LIMITS.chat_limit)
@@ -122,21 +126,21 @@ async def test_user_chatbot_rate_limit(test_client,login_test_user):
             headers=login_test_user) as response:
 
             #check that response given
+            assert response.status_code == 200
             chunks = list(response.iter_text())
+            
             assert all(isinstance(chunk,str) for chunk in chunks)
 
-    response =  test_client.post("/chat",json={
+    response = test_client.post("/chat", json={
             "user_message": user_message,
             "chat_history": test_chat_history,
             "conversation_id": dummy_conversation_id
-            }, headers=login_test_user) 
-    
+            }, headers=login_test_user)
 
     # Ensure that rate limit is enforced for user
-
+    assert response.status_code == 429
     message = response.json()["message"].lower()
     logging.info(f"message: {message}")
-    assert response.status_code== 429
     assert "message allowance reached" in message
 
 
