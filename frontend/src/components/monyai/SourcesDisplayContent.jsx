12a8d99a7cc2a61a18bded6b4d6916d4244e7a42
Taku <tlm2g18@soton.ac.uk>
2026-06-13 14:35:06 +0100

    updated refs streaming and added speech marks to source content

--- frontend/src/components/monyai/SourcesDisplayContent.jsx
+++ frontend/src/components/monyai/SourcesDisplayContent.jsx
@@ -22,7 +22,7 @@ function SourcesDisplayContent(props) {
                     <h4 className="source-name">Source {index + 1}</h4>
                     <a href={source.url} target="_blank" rel="noreferrer">{source.url}</a>
                     <br></br>
-                    <p className="source-content">{source.page_content}</p>
+                    <p className="source-content"><i>"{source.page_content}"</i></p>
                 </div>
             ))}
         </div>
